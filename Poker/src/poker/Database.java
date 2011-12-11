0cdcd0ac76e59f9e9485be1eb8c24f0ef6ff3aa3
Michal Sznajder <msznajder@gmail.com>
2011-12-12 00:04:37 +0100

    dodane wywolanie hashowania hasla przy logowaniu i rejestracji - klasa User dodane sprawdzanie podwojnego wprowadzenia hasla przy rejestracji - klasa RegisterDialog dodane sprawdzanie istnienia usera w bazie przy rejestracji - metoda userCheck w klasie User zmiana klasy kodujacej hasla na hashujaca sha1 z seedem - usuniecie klasy Encode, dodanie klasy Crypto

--- Poker/src/poker/Database.java
+++ Poker/src/poker/Database.java
@@ -8,40 +8,37 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
  * @author siesiek
  */
 public class Database {
+
     private static Connection connection;
-    
-    public static void connect() throws SQLException, IOException{
+    private static String username = "root";
+    private static String password = "";
+    private static String url = "jdbc:mysql://localhost:3306/poker";
+
+    public static void connect() throws SQLException, IOException {
         String drivers = "com.mysql.jdbc.Driver";
-        if(drivers != null){
+        if (drivers != null) {
             System.setProperty("jdbc.drivers", drivers);
         }
-        String url = "jdbc:mysql://localhost:3306/poker";
-        String username = "root";
-        String password = "";
         connection = DriverManager.getConnection(url, username, password);
 //        return DriverManager.getConnection(url, username, password);
     }
-    
-    public static Connection getConnection(){
-        if(connection == null){
-            try {
+
+    public static Connection getConnection() {
+        try{
+            if (connection == null) {
+                    connect();
+            }
+            if(!connection.isValid(0)){
                 connect();
-            } catch (SQLException ex) {
-                Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (IOException ex) {
-                Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
+        catch (Exception e){}
         return connection;
     }
-    
-    
 }
